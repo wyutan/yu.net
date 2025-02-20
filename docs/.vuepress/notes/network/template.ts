0ee07609c5f9b59a8b98475f9cac9972b3d1028d
yu <90786339+wyutan@users.noreply.github.com>
2025-02-20 18:44:36 +0800

    Update template.ts

--- docs/.vuepress/notes/network/template.ts
+++ docs/.vuepress/notes/network/template.ts
@@ -1,11 +1,18 @@
 import { defineNoteConfig } from 'vuepress-theme-plume'
 
 export default defineNoteConfig({
+  text: '模板',
   dir: '/network/template',
-  link: '/network/template/',
+  link: '/network/template',
   sidebar: [
-    'hw_ce',
-    'hw_s',
-    'h3c',
-  ]
-});
+    {
+      prefix: '/network/template',
+      text: '模板',
+      items: [
+        'hw_ce系列',
+        'hw_s系列',
+        'h3c',
+          ]
+    },  
+  ],
+})
