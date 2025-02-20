4a58dc3b89aed645987f90498c4fe30023d10020
yu <90786339+wyutan@users.noreply.github.com>
2025-02-20 18:34:40 +0800

    Update index.ts

--- docs/.vuepress/notes/index.ts
+++ docs/.vuepress/notes/index.ts
@@ -1,7 +1,7 @@
 import { defineNotesConfig } from 'vuepress-theme-plume'
 import protocol from './network/protocol.ts'
 import template from './network/template.ts'
-//import about from './about'
+import about from './about'
 
 export default defineNotesConfig({
   // 声明所有笔记的目录，(默认配置，通常您不需要声明它)
