61339797768087a5b62865cc913f588f6ae26148
yu <90786339+wyutan@users.noreply.github.com>
2025-02-20 17:19:23 +0800

    Create index.ts

---
+++ docs/.vuepress/notes/index.ts
@@ -0,0 +1,15 @@
+import { defineNotesConfig } from 'vuepress-theme-plume'
+import network from './network/index.ts'
+//import about from './about'
+
+export default defineNotesConfig({
+  // 声明所有笔记的目录，(默认配置，通常您不需要声明它)
+  dir: '/',
+  link: '/',
+  // 在这里添加 note 配置
+  notes: [ 
+    协议,
+    模板,
+    about,
+  ]
+})
