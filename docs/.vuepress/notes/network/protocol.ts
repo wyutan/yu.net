7e033e3a2b3929d98e270af520441380903f898a
yu <90786339+wyutan@users.noreply.github.com>
2025-02-20 18:18:50 +0800

    Update and rename network.ts to protocol.ts

---
+++ docs/.vuepress/notes/network/protocol.ts
@@ -0,0 +1,10 @@
+import { defineNoteConfig } from 'vuepress-theme-plume'
+
+export default defineNoteConfig({
+  dir: '/network/protocol',
+  link: '/network/protocol/',
+  sidebar: [
+    'bgp',
+    'ospf'
+  ]
+});
