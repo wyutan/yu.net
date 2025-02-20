ef2d751dd26d7f289a26d1d37fd5dc7af5864e20
yu <90786339+wyutan@users.noreply.github.com>
2025-02-20 21:01:45 +0800

    Update linux.ts

--- docs/.vuepress/notes/os/linux.ts
+++ docs/.vuepress/notes/os/linux.ts
@@ -6,32 +6,12 @@ export default defineNoteConfig({
   link: '/os/linux',
   sidebar: [
     {
-      items: [
-        {
-          text: '常用',
-          dir: '常用',
-          collapsed: false,
-          items: [
-            { text: '重置RHEL7.9密码', link: '重置RHEL密码' },
-            { text: '创建及扩容LVM', link: 'LVM' },
-            { text: '免密登录', link: '免密登录' },
-          ]
-        },
-      ],
-    },
-    {
-      items: [
-        {
-          text: '命令',
-          dir: 'command',
-          collapsed: false,
-          items: [
-            { text: 'vim', link: 'vim' },
-            { text: 'find', link: 'find' },
-            { text: 'sed', link: 'sed' },
-          ]
-        },
-      ],
-    },
-  ]
+      prefix: '/os/linux',
+      text: '',
+      items: [ 
+        { text: 'command', link: '/command', sidebar: 'auto' },
+        { text: '常用', link: '/常用', sidebar: 'auto' },
+      ]
+    },  
+  ],
 })
