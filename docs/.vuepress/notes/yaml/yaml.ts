d55587e0ca2d1f3ce0c2e0a53699acf264473bda
yu <90786339+wyutan@users.noreply.github.com>
2025-02-20 23:47:06 +0800

    更新 yaml.ts

--- docs/.vuepress/notes/yaml/yaml.ts
+++ docs/.vuepress/notes/yaml/yaml.ts
@@ -8,8 +8,8 @@ export default defineNoteConfig({
     {
       prefix: '/yaml/',
       items: [
-        { text: 'mysql', link: 'mysql.md' },
-        { text: 'zabbix', link: 'zabbix.md' },
+        'mysql',
+        'zabbix',
        ]
     },  
   ],
