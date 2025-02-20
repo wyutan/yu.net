d468683d9e1a2a37a3b35e65c8a59a1c969f585a
yu <90786339+wyutan@users.noreply.github.com>
2025-02-20 19:20:00 +0800

    Update plume.config.ts

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -11,7 +11,8 @@ export default defineThemeConfig({
   appearance: true,  // 配置 深色模式
 
   social: [
-    { icon: 'github', link: '/' },
+    { icon: 'gmail', link: 'mailto:wyu.tan99@gmail.com' },
+    // { icon: 'github', link: '/' },
   ],
   // navbarSocialInclude: ['github'], // 允许显示在导航栏的 social 社交链接
   // aside: true, // 页内侧边栏， 默认显示在右侧
