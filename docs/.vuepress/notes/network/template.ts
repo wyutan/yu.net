5d6172fc7ddecf1e7a64e6030687434325a8257b
yu <90786339+wyutan@users.noreply.github.com>
2025-02-20 18:19:38 +0800

    Create template.ts

---
+++ docs/.vuepress/notes/network/template.ts
@@ -0,0 +1,11 @@
+import { defineNoteConfig } from 'vuepress-theme-plume'
+
+export default defineNoteConfig({
+  dir: '/network/template',
+  link: '/network/template/',
+  sidebar: [
+    'hw_ce',
+    'hw_s',
+    'h3c',
+  ]
+});
