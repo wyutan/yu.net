ddc9e27a239e3f736cf71014d375a48dcf2fb800
yu <90786339+wyutan@users.noreply.github.com>
2025-02-20 20:14:33 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,7 +5,7 @@ import { plumeTheme } from 'vuepress-theme-plume'
 export default defineUserConfig({
   base: '/',
   lang: 'zh-CN',
-  title: 'My Vuepress Site',
+  title: '谈呀',
   description: '',
 
   head: [
