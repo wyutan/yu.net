fca4e93a90c43f260406f7226ed86a76eccdb73e
yu <90786339+wyutan@users.noreply.github.com>
2025-02-21 11:57:57 +0800

    Update linux.ts

--- docs/.vuepress/notes/os/linux.ts
+++ docs/.vuepress/notes/os/linux.ts
@@ -2,13 +2,12 @@ import { defineNoteConfig } from 'vuepress-theme-plume'
 
 export default defineNoteConfig({
   text: 'linux',
-  icon: '',
   dir: '/os/linux',
   link: '/os/linux',
   sidebar: [
     {
-      //prefix: '/os/linux',
-      //text: 'linux',
+      prefix: '/os/linux',
+      text: 'linux',
       items: [
         { text: '', link: '', },
         {
