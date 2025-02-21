7f144cdba31d0f1c97574802fcba57adb1407c7f
yu <90786339+wyutan@users.noreply.github.com>
2025-02-21 09:19:45 +0800

    Update navbar.ts

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,7 +1,7 @@
 import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export const navbar = defineNavbarConfig([
-  { text: '首页', link: '/' },
+  { text: '首页', icon: 'mdi:home', link: '/' },
   //{ text: '博客', link: '/blog/' },
   //{ text: '标签', link: '/blog/tags/' },
   //{ text: '归档', link: '/blog/archives/' },
