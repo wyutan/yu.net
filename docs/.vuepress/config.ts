c32fe2a4e83c9aa540a82fadd5d785f05ee59281
yu <90786339+wyutan@users.noreply.github.com>
2025-02-22 20:41:22 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -67,51 +67,8 @@ export default defineUserConfig({
     plugins: {
       docsearch:{
         appId: '581EUIWCRI',
-        apiKey: 'bca7476a6151d3bbf482da4caef381ec',
-        indexName: 'git-x-cli',
-        locales: {
-          "/": {
-            placeholder: "搜索",
-            translations: {
-              button: {
-                buttonText: "搜索",
-                buttonAriaLabel: "搜索",
-              },
-              modal: {
-                searchBox: {
-                  resetButtonTitle: "清除查询条件",
-                  resetButtonAriaLabel: "清除查询条件",
-                  cancelButtonText: "取消",
-                  cancelButtonAriaLabel: "取消",
-                },
-                startScreen: {
-                  recentSearchesTitle: "搜索历史",
-                  noRecentSearchesText: "没有搜索历史",
-                  saveRecentSearchButtonTitle: "保存至搜索历史",
-                  removeRecentSearchButtonTitle: "从搜索历史中移除",
-                  favoriteSearchesTitle: "收藏",
-                  removeFavoriteSearchButtonTitle: "从收藏中移除",
-                },
-                errorScreen: {
-                  titleText: "无法获取结果",
-                  helpText: "你可能需要检查你的网络连接",
-                },
-                footer: {
-                  selectText: "选择",
-                  navigateText: "切换",
-                  closeText: "关闭",
-                  searchByText: "搜索提供者",
-                },
-                noResultsScreen: {
-                  noResultsText: "无法找到相关结果",
-                  suggestedQueryText: "你可以尝试查询",
-                  reportMissingResultsText: "你认为该查询应该有结果？",
-                  reportMissingResultsLinkText: "点击反馈",
-                },
-              },
-            },
-          },
-        },
+        apiKey: '61f2ec0768a5f96ee678605d56e976d8',
+        indexName: 'git_x_cli_net',
       },
       'vuepress-plugin-iconify': {
         size: '24px',  // 图标的默认大小
