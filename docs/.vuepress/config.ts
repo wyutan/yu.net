412fa2e52fc127e7069943be6141afaa814ee3e6
yu <90786339+wyutan@users.noreply.github.com>
2025-02-23 17:08:46 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -91,11 +91,11 @@ export default defineUserConfig({
        * 启用此搜索需要将 本地搜索 search 设置为 false
        * @see https://theme-plume.vuejs.press/config/plugins/search/#algolia-docsearch
        */
-      docsearch: {
+      docsearchPlugin: {
         appId: '5FYYWUK4F4',
         apiKey: 'e6476b6808afeb8446c9cfd429df36ea',
         indexName: 'github',
-      },
+      },      
 
       /* 文章字数统计、阅读时间，设置为 false 则禁用 */
       // readingTime: true,
