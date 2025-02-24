936dffbe62aee5166e308a90f6e1a6ff84faa6bb
yu <90786339+wyutan@users.noreply.github.com>
2025-02-25 00:19:05 +0800

    更新 navbar.ts

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -5,15 +5,7 @@ export const navbar = defineNavbarConfig([
   //{ text: '博客', link: '/blog/' },
   //{ text: '标签', link: '/blog/tags/' },
   //{ text: '归档', link: '/blog/archives/' },
-  {
-    text: '网络',
-    icon: 'lucide:network',
-    link: '/network/',
-    //items: [
-      //{ text: '协议', icon: '', link: '/network/protocol/', },
-      //{ text: '', icon: '', link: '/network/', },
-    //]
-  },
+  { text: '网络', icon: 'lucide:network', link: '/network/', },
   {
     text: 'os',
     icon: 'ix:operating-system',
@@ -21,14 +13,7 @@ export const navbar = defineNavbarConfig([
       { text: 'linux', icon: 'skill-icons:linux-light', link: '/os/linux/', },
     ]
   }, 
-  {
-    text: '容器',
-    icon: 'carbon:container-services',
-    items: [
-      { text: 'docker', icon: 'logos:docker-icon', link: '/容器/docker/', },
-      { text: 'k8s', icon: '', link: '/容器/k8s/', },
-    ]
-  },
+  { text: '容器', icon: 'carbon:container-services', link: '/容器/', },
   { text: '疑难杂症', icon: 'icon-park-outline:thinking-problem',  link: '/疑难杂症/疑难杂症.md', },
   { text: '关于', icon: 'clarity:heart-line',  link: '/about/', },
 ])
