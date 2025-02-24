0d990e13dfc3c25e31c8709cbc274c3ce7d6fdca
yu <90786339+wyutan@users.noreply.github.com>
2025-02-25 00:28:36 +0800

    Delete docs/.vuepress/notes/容器/docker.ts

--- docs/.vuepress/notes/容器/docker.ts
+++
@@ -1,9 +0,0 @@
-import { defineNoteConfig } from 'vuepress-theme-plume'
-
-export default defineNoteConfig({
-  dir: '/容器/',
-  link: '/容器/',
-  sidebar: [
-    { text: 'docker笔记', prefix: 'docker', collapsed: false, items: 'auto', },
-  ],
-})
