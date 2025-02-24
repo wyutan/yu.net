692756e67b4a6aaf80f44be63ce176f4b9456f68
yu <90786339+wyutan@users.noreply.github.com>
2025-02-24 23:33:27 +0800

    更新 docker.ts

--- docs/.vuepress/notes/容器/docker.ts
+++ docs/.vuepress/notes/容器/docker.ts
@@ -1,8 +1,8 @@
 import { defineNoteConfig } from 'vuepress-theme-plume'
 
 export default defineNoteConfig({
-  dir: '/容器/docker',
-  link: '/容器/docker',
+  dir: '/容器/',
+  link: '/容器/',
   sidebar: [
     { text: 'docker笔记', prefix: 'docker', collapsed: false, items: 'auto', },
   ],
