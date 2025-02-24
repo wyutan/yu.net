592e70407c51a34c75027ba1d8f5d5ca884da853
yu <90786339+wyutan@users.noreply.github.com>
2025-02-25 00:30:12 +0800

    更新 容器.ts

--- docs/.vuepress/notes/容器.ts
+++ docs/.vuepress/notes/容器.ts
@@ -4,8 +4,8 @@ export default defineNoteConfig({
   dir: '/容器/',
   link: '/容器/',
   sidebar: [
-    { text: 'Docker 笔记', prefix: 'docker', collapsed: false, items: 'auto', },
+    { text: 'Docker 笔记', prefix: 'docker', collapsed: true, items: 'auto', },
     //{ text: 'Kubernetes 笔记', prefix: 'k8s', collapsed: false, items: 'auto', },
-    { text: 'YAML', prefix: 'k8s/yaml', collapsed: false, items: 'auto', },
+    { text: 'YAML', prefix: 'k8s/yaml', collapsed: true, items: 'auto', },
   ],
 })
