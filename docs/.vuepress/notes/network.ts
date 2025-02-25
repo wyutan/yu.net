c8e6b5226ccf2b8f69cd8bd5333fa76b86728d8b
yu <90786339+wyutan@users.noreply.github.com>
2025-02-25 14:54:13 +0800

    Update network.ts

--- docs/.vuepress/notes/network.ts
+++ docs/.vuepress/notes/network.ts
@@ -5,13 +5,7 @@ export default defineNoteConfig({
   link: '/network/',
   sidebar: [
     {text: '配置模板', prefix: 'template', collapsed: false, items: 'auto', },
-  ],
-}),
-
-export default defineNoteConfig({
-  dir: '/network/',
-  link: '/network/',
-  sidebar: [
     { text: '网络协议', prefix: 'protocol', collapsed: false, items: 'auto', },
+
   ],
 })
