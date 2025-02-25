1ca194cc6825667b91f533f9ff8e523df36b0f0f
yu <90786339+wyutan@users.noreply.github.com>
2025-02-25 08:28:04 +0800

    Update and rename docs/.vuepress/notes/os/linux.ts to docs/.vuepress/notes/linux.ts

---
+++ docs/.vuepress/notes/linux.ts
@@ -0,0 +1,11 @@
+import { defineNoteConfig } from 'vuepress-theme-plume'
+
+export default defineNoteConfig({
+  dir: '/os/linux',
+  link: '/os/linux',
+  sidebar: [
+      { text: 'linux学习笔记', link: '/os/linux/' },
+      { text: 'linux命令', prefix: 'command', collapsed: false, items: 'auto',},
+      { text: '常用功能', prefix: '常用', collapsed: false, items: 'auto',},
+          ]
+})
