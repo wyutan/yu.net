066bcadefa2df84cc8b65d0f13da68e0dffd7496
yu <90786339+wyutan@users.noreply.github.com>
2025-03-17 12:32:32 +0800

    Update index.ts

--- docs/.vuepress/notes/index.ts
+++ docs/.vuepress/notes/index.ts
@@ -1,6 +1,7 @@
 import { defineNotesConfig } from 'vuepress-theme-plume'
 import network from './network.ts'
 import linux from './linux.ts'
+import cloud from './cloud.ts'
 import 容器 from './容器.ts'
 import about from './about'
 
@@ -12,6 +13,7 @@ export default defineNotesConfig({
   notes: [
     network,
     linux,
+    cloud,
     容器,
     about,
   ]
