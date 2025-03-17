b2a0fd2b8faa1a13c856d55a9560bc205ced1fca
yu <90786339+wyutan@users.noreply.github.com>
2025-03-17 14:46:09 +0800

    Update cloud.ts

--- docs/.vuepress/notes/cloud.ts
+++ docs/.vuepress/notes/cloud.ts
@@ -5,6 +5,6 @@ export default defineNoteConfig({
   link: '/os/cloud',
   sidebar: [
     { text: '云计算学习笔记', link: '/os/cloud/', },
-    { text: '云计算笔记', prefix: 'note', collapsed: false, items: 'auto', },
+    { text: '', prefix: 'note', collapsed: false, items: 'auto', },
   ]
 })
