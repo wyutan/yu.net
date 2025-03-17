d95c363931ab9a5a40d1760a8183d22b94e6baf6
yu <90786339+wyutan@users.noreply.github.com>
2025-03-17 14:21:26 +0800

    Update linux.ts

--- docs/.vuepress/notes/linux.ts
+++ docs/.vuepress/notes/linux.ts
@@ -5,6 +5,7 @@ export default defineNoteConfig({
   link: '/os/linux',
   sidebar: [
     { text: 'linux学习笔记', link: '/os/linux/' },
+    { text: 'linux笔记', prefix: 'note', collapsed: false, items: 'auto', },
     { text: 'linux命令', prefix: 'command', collapsed: false, items: 'auto', },
     { text: '常用功能', prefix: '常用', collapsed: false, items: 'auto', },
   ]
