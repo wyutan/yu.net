eeaed5f5ddba67bdeeb8b6de38703d7dd19c5cb6
tanya <90786339+wyutan@users.noreply.github.com>
2025-04-17 13:44:00 +0800

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -28,7 +28,7 @@ export default defineUserConfig({
     /* 页内信息 */
     // editLink: true,
     // lastUpdated: true,
-    contributors: true,
+    contributors: false,
     docsRepo: 'https://github.com/wyutan/tanya/',
     docsDir: 'docs',
     changelog: { maxCount: 10 },
